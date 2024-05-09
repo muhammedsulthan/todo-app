35075c310f3f1a35e39260b64c1c1fa7a94e97d0
salman subair <muhammedsulthanziz@gamil.com>
2024-05-09 13:51:34 +0530

    intial commet

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import TodoApp from './component/TodoApp/TodoApp';
+import About from './component/About/About';
+import Header from './component/Header/Header';
+import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
+
+
+export default () => { 
+
+    return(
+  
+      <Router>
+        <Header />
+      <Routes>
+        <Route path='/'exact Component={TodoApp} />
+        <Route path='/About' Component={About} />
+         
+        </Routes>
+
+      </Router>
+  
+
+    )
+  
+}
+
